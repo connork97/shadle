fdb6325ddfc2a13e69aac7c0d12497558e6882d5
Connor Kormos <connorkormos97@gmail.com>
2023-09-06 14:36:34 -0700

    setting database uri

--- server/app.py
+++ server/app.py
@@ -22,7 +22,7 @@ app.config.from_object('config')
 # from flask_bcrypt import Bcrypt
 
 # app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # db = SQLAlchemy(app)
