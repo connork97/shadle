3be65ac0767c7cafacbb60251b043842706ce809
Connor Kormos <connorkormos97@gmail.com>
2026-09-08 16:57:19 -0700

    new public domain for api

--- client/src/api/fetch.js
+++ client/src/api/fetch.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:3000';
-export const BASE_URL = "https://shadle-api.railway.internal";
+// export const BASE_URL = "https://shadle-api.railway.internal";
+export const BASE_URL = "shadle-api-production.up.railway.app";
